8a83d75c4c05bc9ba5d136f0937f8782456f0814
tekatheewok <happygamer4700@gmail.com>
2025-01-28 08:14:23 -0500

    more code

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
-localStorage.setItem("colton", "skiddev")
+let clicks = localStorage.getItem("clicks")
+let rebirths = localStorage.getItem("rebirths")
 
-console.log(localStorage.getItem("colton"))
+function initialize() {
+    if (clicks === null) {
+        clicks = 0
+    };
+
+    if (rebirths === null) {
+        rebirths = 0
+    };
+    
+    if (typeof(clicks) === "string") {
+        Number(clicks)
+    };
+
+    if (typeof(rebirths) === "string") {
+        Number(rebirths)
+    };
+
+    document.getElementById("click").onclick = addClick()
+};
+
+function addClick() {
+    let clickInc = getClickIncrease()
+};
+
+function getClickIncrease() {
+    
+};
+
+function savePlayerData() {
+    localStorage.setItem("clicks", toString(clicks))
+    localStorage.setItem("rebirths", toString(rebirths))
+}
+
+initialize()
